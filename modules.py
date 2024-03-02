344b2193be5cafb08b85da29c383f476b53d1a0e
jeffreylaichunghang <laichunghangjeffrey@gmail.com>
2024-03-02 19:29:06 +0800

    modules basic

---
+++ modules.py
@@ -0,0 +1,7 @@
+import math
+import random
+from string import ascii_letters
+
+print(math.sqrt(4))
+print(random.randint(0,10))
+print(ascii_letters)
---
+++ modules/main.py
@@ -0,0 +1,12 @@
+# import support
+# from support import Test, var, add_2_numbers
+from support import *
+
+# test = support.Test()
+# print(test.a)
+# test.some_method()
+# print(support.var)
+# print(support.add_2_numbers(1, 2))
+test = Test()
+print(add_2_numbers(1, 2))
+print(var)
