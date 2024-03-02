bf7b067ee66d5148ac1e5288857823fd902065b2
jeffreylaichunghang <laichunghangjeffrey@gmail.com>
2024-03-02 14:05:30 +0800

    simple functions and methods in python

---
+++ basics.py
@@ -0,0 +1,12 @@
+# variables (multiple lines for a statement)
+test = 123
+test = test + 50
+test += 50
+# print(test)
+
+# input
+# user_input = input("Please write something")
+# print(user_input)
+
+user_name = input('What is your name? ')
+print('Your name is {user_name}')
---
+++ functions.method.py
@@ -0,0 +1,12 @@
+# functions
+
+# methods -> functions of datatypes
+print('value'.upper())
+print('VALUE'.lower())
+print('value'.replace('e', '3'))
+
+# new functions
+print(abs(-1))
+print(max(10, 5))
+print(len('value'))
+print(type('value'))
